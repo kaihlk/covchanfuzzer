d54054be4aed7ebd987fe770a04137e5cbee2f51
Kai <kai@localhost.localdomain>
2023-04-18 00:32:12 +0200

    Scapy Installation added

--- shell-scripts/vm_cs_setup.sh
+++ shell-scripts/vm_cs_setup.sh
@@ -2,3 +2,8 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
+pip install -U  matplotlib
+pip install -U pyx
+sudo apt install -U  graphviz
+pip install -U cryptograpy
+pip install -U scapy
