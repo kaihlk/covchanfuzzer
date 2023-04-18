48e9f162d96814164bda3df6de373052c28768eb
Kai <kai@localhost.localdomain>
2023-04-18 23:54:50 +0200

    wireshark boofuzz + enviroments

--- shell-scripts/vm_cs_setup.sh
+++ shell-scripts/vm_cs_setup.sh
@@ -1,9 +1,33 @@
+#Update System
 sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
+cd ~
+mkdir covchanfuzzer
+cd covchanfuzzer
+#Install Scapy
+mkdir scapy
+cd scapy
+python3 -m venv scapy-env 
+source scapy-env/bin/activate
 pip install -U  matplotlib
 pip install -U pyx
-sudo apt install -U  graphviz
+sudo apt install graphviz
 pip install -U cryptograpy
 pip install -U scapy
+deactivate
+cd ..
+#Install Wireshark
+sudo apt install wireshark
+sudo usermod -a -G wireshark kai
+#Install boofuzz
+cd ~/covchanfuzzer
+mkdir boofuzz
+cd boofuzz
+python3 -m venv boofuzz-env 
+source boofuzz-env/bin/activate
+pip install -U boofuzz
+deactivate
+cd ..
+
