f22d31d8d03dab41d659420ae1eadeee9e35320a
kai <kai@localhost.localdomain>
2023-04-23 22:23:27 +0200

    Conda

--- shell-scripts/vm_cs_setup.sh
+++ shell-scripts/vm_cs_setup.sh
@@ -3,33 +3,38 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
-cd ~
-mkdir covchanfuzzer
-cd covchanfuzzer
+#cd ~
+#mkdir covchanfuzzer
+#cd covchanfuzzer
 #Install Scapy
-mkdir scapy
-cd scapy
-python3 -m venv scapy-env 
-source scapy-env/bin/activate
-pip install -U  matplotlib
-pip install -U pyx
+#mkdir scapy
+#cd scapy
+#python3 -m venv scapy-env 
+#source scapy-env/bin/activate
+
+#install anaconda
+conda activate env-scapy
+
+conda install  matplotlib
+conda install pyx
 sudo apt install graphviz
-pip install -U cryptograpy
-pip install -U scapy
-pip freeze > requirements.txt
+conda install cryptograpy
+conda install  scapy
+conda list --export  > requirements.txt
 deactivate
 cd ~
 #Install Wireshark
 sudo apt install wireshark
 sudo usermod -a -G wireshark kai
 #Install boofuzz
-cd ~/covchanfuzzer
-mkdir boofuzz
-cd boofuzz
-python3 -m venv boofuzz-env 
-source boofuzz-env/bin/activate
-pip install -U boofuzz
-pip freeze > requirements.txt
+
+##cd ~/covchanfuzzer
+##mkdir boofuzz
+##cd boofuzz
+##python3 -m venv boofuzz-env 
+conda activate env-boofuzz
+conda install boofuzz
+pip freezeconda list --export  > requirements.txt
 deactivate
 cd ~
 
