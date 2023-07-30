00f7e48d8b71d22bdaed9880805e4875add65680
Kai <kai.hoelk@gmx.de>
2023-07-30 15:04:13 +0200

    Update CC Architecture

---
+++ scripts/http1_request_builder.py
@@ -0,0 +1,139 @@
+# http1_covered_channels.py
+
+# Definition of functions to generate cover channels in http/1.1 requests
+# TODO HTTP/2 request generation, with method:
+
+import random
+from urllib.parse import quote
+
+class HTTP1_Request_Builder:
+    def __init__(self, target_host, target_port):
+        self.target_host = target_host
+        self.target_port = target_port   
+        self. default_headers = [
+            # The field with the Host and the url must be generated and inserted in the functions
+            (
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            ),
+            (
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            ),
+            ("Accept-Encoding", "gzip, deflate, br"),
+            ("Accept-Language", "en-US,en;q=0.9"),
+            ("Connection", "keep-alive"),
+        ]
+        self.default_headers_firefox = [
+            # The field with the Host and the url must be generated and inserted in the functions
+            (
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            ),
+            (
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            ),
+            ("Accept-Encoding", "gzip, deflate, br"),
+            ("Accept-Language", "en-US,en;q=0.9"),
+            ("Connection", "keep-alive"),
+        ]
+        self.default_headers_chromium = [
+            # The field with the Host and the url must be generated and inserted in the functions
+            (
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            ),
+            (
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            ),
+            ("Accept-Encoding", "gzip, deflate, br"),
+            ("Accept-Language", "en-US,en;q=0.9"),
+            ("Connection", "keep-alive"),
+        ]
+        self.default_headers_safari = [
+            # The field with the Host and the url must be generated and inserted in the functions
+            (
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            ),
+            (
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            ),
+            ("Accept-Encoding", "gzip, deflate, br"),
+            ("Accept-Language", "en-US,en;q=0.9"),
+            ("Connection", "keep-alive"),
+        ]
+    def parse_host(self):
+        '''Parse host uris'''
+        # Initialize variables
+        scheme = ""
+        subdomain = ""
+        hostname = ""
+        domain = ""
+        port = ""
+        path = ""
+        host = self.target_host
+        # Check if the host contains a scheme
+        if "://" in host:
+            # Split the host into scheme and the rest of the URL
+            parts = host.split("://", 1)
+            scheme = parts[0]
+            remaining = parts[1]
+        else:
+            remaining = host
+
+        # Split the remaining URL into subdomain, hostname, domain, and port (if present)
+        parts = remaining.split("/", 1)
+        if len(parts) == 2:
+            remaining, path = parts
+
+        subdomain_parts = remaining.split(".", 1)
+        if len(subdomain_parts) == 2:
+            subdomain = subdomain_parts[0]
+            remaining = subdomain_parts[1]
+
+        hostname_parts = remaining.split(".", 1)
+        if len(hostname_parts) == 2:
+            hostname = hostname_parts[0]
+            domain_port = hostname_parts[1].split(":", 1)
+            domain = domain_port[0]
+            if len(domain_port) == 2:
+                port = domain_port[1]
+
+        # Return the extracted parts as a tuple
+        return scheme, subdomain, hostname, domain, port, path
+
+
+    def generate_request(self, 
+        host, port, url="/", method="GET", headers=None, fuzzvalue=None
+    ):
+        '''Generation of request package without insertion of a covert channel'''
+        # Check if headers are provided elsewise take default headers
+        if headers is None:
+            headers = default_headers.copy()
+        else:
+            # Create a copy to avoid modifying the original list
+            headers = headers.copy()
+
+        # Insert the Host header at the beginning of the list
+        headers.insert(0, ("Host", host))
+
+        # Build the request_line from the provided arguments
+        request_line = f"{method} {url} HTTP/1.1\r\n"
+
+        # Build the request from request line and headers
+        request_string = request_line
+        for header in headers:
+            request_string += f"{header[0]}: {header[1]}\r\n"
+
+        # Add the ending of the request
+        request_string += "\r\n"
+
+        # No deviation
+        deviation_count = 0
+        return request_string, deviation_count
+
+
--- scripts/main.py
+++ scripts/main.py
@@ -4,9 +4,31 @@ from runner import ExperimentRunner
 
 
 
+class_mapping_requests ={
+    1: HTTP1_Request_Builder,
+    2: HTTP1_Request_CC_Case_Insensitivity,
+    3: HTTP1_Request_CC_Random_Whitespace,
+    4: HTTP1_Request_CC_Reordering_Header_Fields,
+    5: HTTP1_Reqeust_CC_URI_Represenation,
+    6: HTTP1_Request_CC_URI_Case_Insentivity,
+    7: HTTP1_Reqeust_CC_URI_Hex_Hex,
+}
+
+class_mapping_connection = {
+    1: HTTP1_TCP_Connection,
+    2: HTTP1_TLS_Connection,
+    3: HTTP1_CC_TCP2TLS_Upgrade,
+    4: HTTP2_TLS_Connection
+    }
+
+class_mapping_timing  = {
+    1: Standard,
+    2: Frequency_Modulation,
+    3: Amplitude_Modulation,
+}
 
 def main():
-    '''Function that runs the connection, selection of the CC and the fuzzer, should be cleaned up and externalised'''
+    '''Function that runs the connection, selection of the CC and the fuzzer'''
     # TODO
     # host and channelselction, number of attempts as arguments?
 
@@ -18,7 +40,9 @@ def main():
     # Experiment Configuration Values
     experiment_configuration = {
         "comment": "Some text describing the Testrun",
-        "covertchannel_number": 3,
+        "covertchannel_request_number": 3,
+        "covertchannel_connection_number": 1,
+        "covertchannel_timing_number": 1,
         "num_attempts": 100,
         "conn_timeout": 20.0,
         "nw_interface": "lo",
