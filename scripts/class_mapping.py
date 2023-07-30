6c7ab1a0a53a88fc85379ce52356ab84ece28c31
Kai <kai.hoelk@gmx.de>
2023-07-30 18:42:29 +0200

    Save Covert Channel Names

--- scripts/class_mapping.py
+++ scripts/class_mapping.py
@@ -5,11 +5,11 @@ from http1_covered_channels import *
 requests_builders ={
     1: HTTP1_Request_Builder,
     2: HTTP1_Request_CC_Case_Insensitivity,
- #   3: HTTP1_Request_CC_Random_Whitespace,
- #   4: HTTP1_Request_CC_Reordering_Header_Fields,
- #   5: HTTP1_Reqeust_CC_URI_Represenation,
- #   6: HTTP1_Request_CC_URI_Case_Insentivity,
- #   7: HTTP1_Reqeust_CC_URI_Hex_Hex,
+    3: HTTP1_Request_CC_Random_Whitespace,
+    4: HTTP1_Request_CC_Reordering_Header_Fields,
+    5: HTTP1_Request_CC_URI_Represenation,
+    6: HTTP1_Request_CC_URI_Case_Insentivity,
+    7: HTTP1_Reqeust_CC_URI_Hex_Hex,
 }
 
 """ class_mapping_connection = {
