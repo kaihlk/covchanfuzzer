c8703ba1e9a85bb3935ddb284471283acdcab671
Kai <kai.hoelk@gmx.de>
2023-08-02 01:05:55 +0200

    Collected Requests for Standard Browser

---
+++ shell-scripts/curlssl.sh
@@ -0,0 +1,14 @@
+SSLKEYLOGFILE=home/kai/git/pcap/curlssl.log curl https://www.example.com
+
+Add path to wireshark TLS Premaster Secret Path
+
+
+Firefox
+export SSLKEYLOGFILE=$HOME/Desktop/keylogfile.txt
+
+firefox
+
+
+Firefox Force HTTP1
+about:config
+network.http.http2.enabled
