774d99f560340aedfbb83e43c8b2515c00900fa9
kaihlk <kai.hoelk@studium.fernuni-hagen.de>
2023-10-13 19:50:59 +0200

    ExpStats

--- exp_analyzer.py
+++ exp_analyzer.py
@@ -20,10 +20,10 @@ class Domain_Response_Analyzator():
             path+"/experiment_stats.csv")
         self.data_frame_prerequest_stats = pandas.read_csv(
             path+"/prerequests.csv")
-        self.data_frame_uri = pandas.read_csv(
-            path+"/uri_dev_statuscode.csv")
-        self.data_frame_rel_uri = pandas.read_csv(
-            path+"/rel_uri_dev_statuscode.csv")        
+        ##self.data_frame_uri = pandas.read_csv(
+        #s    path+"/uri_dev_statuscode.csv")
+        #self.data_frame_rel_uri = pandas.read_csv(
+        #    path+"/rel_uri_dev_statuscode.csv")        
         self.experiment_configuration=self.load_exp_outcome(self.exp_path)
         self.dra_logging=logging.getLogger("main.runner.dra_logger")
         
@@ -59,10 +59,10 @@ class Domain_Response_Analyzator():
         self.save_exp_analyzer_results(host_statistics, prerequest_statistics)
         self.plot_deviation_count_distribution(self.data_frame_prerequest_stats)
         self.plot_2xx_over_attempt_no(self.data_frame_prerequest_stats)
-        self.plot_uri_deviation_count_distribution(self.data_frame_uri)
-        self.plot_rel_uri_deviation_distribution(self.data_frame_rel_uri)
-        self.plot_scatter_prerequest(self.data_frame_rel_uri)
-        
+        #self.plot_uri_deviation_count_distribution(self.data_frame_uri)
+        #self.plot_rel_uri_deviation_distribution(self.data_frame_rel_uri)
+        #self.plot_scatter_prerequest(self.data_frame_rel_uri)
+        self.plot_hosts_responses(self.data_frame_exp_stats)
         
         
         return
@@ -150,29 +150,21 @@ class Domain_Response_Analyzator():
         mpl.savefig(self.exp_path+'/exp_stats_2xx_no_development.png', dpi=300, bbox_inches='tight')
         #mpl.show()
         return
-
+#
     
     def plot_deviation_count_distribution(self, data_frame):
         """Plot 4"""
-        #deviation_count = data_frame['deviation_count']
-        
         deviation_count = data_frame['deviation_count'].values
-        relative_deviation_count = deviation_count / self.experiment_configuration["num_attempts"]*100
-
-    
+        #relative_deviation_count = deviation_count / self.experiment_configuration["num_attempts"]*100  
         # Create a histogram
         mpl.figure(figsize=(10, 8))
-        sns.histplot(relative_deviation_count, kde=True, color='blue', bins=100, label='Data Distribution')
-
-    
-
+        sns.histplot(x=deviation_count, stat='percent', kde=True, color='blue', bins=100, label='Data Distribution')#b s
+        mpl.ylim(0, 100)
         mpl.xlabel('Deviations from original Request')
         mpl.ylabel('Share of Requests (%)')
         mpl.title('Histogram: Deviation Count Distribution')
         mpl.legend()
-        mpl.grid(True)
-
-        
+        mpl.grid(True)       
         mpl.savefig(self.exp_path+'/exp_stats_deviation_distribution.png', dpi=300, bbox_inches='tight')
         # Show the plot or save it to a file
         #mpl.show()
@@ -201,7 +193,7 @@ class Domain_Response_Analyzator():
         mpl.savefig(self.exp_path+'/exp_stats_uri_deviation_distribution.png', dpi=300, bbox_inches='tight')
     
     def plot_rel_uri_deviation_distribution(self, data_frame):
-        
+        """Plot 4.2"""
         
         deviation_counts = data_frame['Relative Deviation'].values
         sums=data_frame['Sum'].values
@@ -258,15 +250,215 @@ class Domain_Response_Analyzator():
         mpl.savefig(self.exp_path+'/exp_stats_host_statuscode.png',
                     dpi=300, bbox_inches='tight')
         #mpl.show()
+    def plot_hosts_responses(self, dataframe, num_clusters=100):
+        file_path=self.exp_path+"/sortet_data.csv"
+        
+        #Extract Data
+        df=dataframe[['Host', '1xx', '2xx', '3xx', '4xx', '5xx', '9xx']]
+        #10 is not constant????
+        df.loc[:, ['1xx', '2xx', '3xx', '4xx', '5xx', '9xx']] /= 10      
 
+        df = df.sort_values(by=['2xx', '1xx', '3xx', '4xx', '5xx', '9xx'], ascending=[False, False, False, False, False, False])
+
+        # Divide Host into Cluster
+        num_clusters = 100
+        cluster_size = len(df) // num_clusters
+        df['Cluster'] = numpy.repeat(range(num_clusters), cluster_size)
+        
+            
+        # Sums per Cluster
+        clustered_data = df.groupby('Cluster').sum()
+        clustered_data.to_csv(file_path, index=False)
+
+        #Build Diagram
+        mpl.style.use('fivethirtyeight')
+        colors = ['#9B59B6', '#2ECC71', '#F1C40F', '#E74C3C',  '#3498DB',   '#34495E']
+        ax = clustered_data.plot(kind='bar',  stacked=True, figsize=(12, 6),width=1, color=colors )
+        
+        mpl.xlabel('Cluster (100)')
+        xticks = numpy.arange(0, num_clusters+1, 10)
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xticks)
+        mpl.ylabel('Share of Response Codes')
+        mpl.title('Response Codes over Host Cluster')
+        mpl.legend(title='Response Codes')
+
+        mpl.show()
+
+
+        """ 
+        # Normalize response code values by dividing by the total response count for each host
+        response_codes = ['1xx','2xx', '3xx', '4xx', '5xx', '9xx']
+        df[response_codes] = df[response_codes].div(df[response_codes].sum(axis=1), axis=0)
+
+        # Perform K-means clustering with 100 clusters
+        n_clusters = 100
+        kmeans = KMeans(n_clusters=n_clusters)
+        df['Cluster'] = kmeans.fit_predict(df[response_codes])
+        
+        
+        # Create a stacked bar plot
+        mpl.figure(figsize=(12, 6))
+
+        bottom = 0
+        for col in df.columns[6:11]:
+            mpl.bar(df['Host'], df[col], label=col, bottom=bottom)
+            bottom += df[col]
+        
+        # Set labels and title
+        mpl.xlabel('Hosts')
+        mpl.ylabel('Share of Response Codes')
+        mpl.title('Distribution of Response Codes by Host')
+
+        # Show the plot
+        mpl.xticks(rotation=45, ha='right')
+        mpl.legend(loc='upper right')
+        mpl.tight_layout()
+        mpl.show() """
+        """ 
+        data_frame = df.copy()
+        response_codes = ['1xx', '2xx', '3xx', '4xx', '5xx', '9xx']
+        
+        # Calculate the proportion of each response code for each host
+        for code in response_codes:
+            data_frame[code] = data_frame[code] / data_frame[response_codes].sum(axis=1)
+
+        # Create clusters based on the distribution of response codes
+        cluster_size = len(data_frame) // num_clusters
+        clusters = [data_frame[i:i + cluster_size] for i in range(0, len(data_frame), cluster_size)]
+
+        # Initialize a list to store the bottom values for stacking
+        bottom_values = [0] * len(clusters)
+
+        # Create a figure and axis
+        fig, ax = mpl.subplots(figsize=(12, 6))
+
+        # Iterate through response codes and plot stacked bars for each code
+        x = numpy.arange(len(clusters))
+        width = 0.8
+        for i, code in enumerate(response_codes):
+            y = [cluster[code].mean() * 100 for cluster in clusters]
+            ax.bar(x, y, width, label=f'Statuscode {code}', bottom=bottom_values)
+            bottom_values = [bottom + val for bottom, val in zip(bottom_values, y)]
+
+        # Remove x-axis tick labels
+        ax.set_xticklabels([])
+
+        # Set the legend
+        ax.legend()
+
+        # Set labels and title
+        ax.set_xlabel('Host Clusters')
+        ax.set_ylabel('Statuscode Share (%)')
+        ax.set_title('Distribution of Statuscodes in Host Clusters')
+
+        mpl.tight_layout()
+
+        # Save or display the plot
+        mpl.savefig(self.exp_path + '/exp_stats_host_statuscode_clusters_stacked.png', dpi=300, bbox_inches='tight')
+        # mpl.show()
+        """
+
+        
+        """ 
+        data_frame = df.copy()
+        response_codes = ['1xx', '2xx', '3xx', '4xx', '5xx', '9xx']
+        
+        # Calculate the proportion of each response code for each host
+        for code in response_codes:
+            data_frame[code] = data_frame[code] / data_frame[response_codes].sum(axis=1)
+
+        # Create clusters based on the distribution of response codes
+        cluster_size = len(data_frame) // num_clusters
+        clusters = [data_frame[i:i + cluster_size] for i in range(0, len(data_frame), cluster_size)]
+
+        # Initialize a list to store cluster labels
+        cluster_labels = []
+
+        # Calculate cluster labels based on the dominant response code
+        for cluster in clusters:
+            dominant_response_code = cluster[response_codes].idxmax(axis=1).value_counts().idxmax()
+            cluster_labels.append(f'Dominant: {dominant_response_code}')
+
+        # Create a figure and axis
+        fig, ax = mpl.subplots(figsize=(12, 6))
+
+        # Plot the bars for each cluster
+        x = numpy.arange(len(cluster_labels))
+        width = 0.15
+        for i, code in enumerate(response_codes):
+            y = [cluster[code].mean() * 100 for cluster in clusters]
+            ax.bar(x + i * width, y, width, label=f'Statuscode {code}')
+
+        # Set the x-axis labels as cluster labels
+        ax.set_xticks(x + 2.5 * width)
+        ax.set_xticklabels(cluster_labels, rotation=45, ha='right')
+
+        # Set the legend
+        ax.legend()
+
+        # Set labels and title
+        ax.set_xlabel('Host Clusters')
+        ax.set_ylabel('Average Statuscode Share (%)')
+        ax.set_title('Distribution of Statuscodes in Host Clusters')
+
+        mpl.tight_layout()
+
+        # Save or display the plot
+        mpl.savefig(self.exp_path + '/exp_stats_host_statuscode_clusters.png', dpi=300, bbox_inches='tight')
+        # mpl.show()
+         """
+        
+        
+        
+        
+        
+        """ # Sort the DataFrame by 1xx, 3xx, 4xx, 5xx, 9xx, and 2xx columns in descending order
+        data_frame = df.sort_values(by=['1xx', '3xx', '4xx', '5xx', '9xx', '2xx'], ascending=[False, False, False, False, False, True])
+
+        hosts = data_frame['Host']
+        response_codes = ['1xx', '3xx', '4xx', '5xx', '9xx', '2xx']
+        colors = ['blue', 'orange', 'red', 'purple', 'gray', 'green']
+        
+        host_count = len(hosts)
+
+        # Create an array of x-values for the bars
+        x = numpy.arange(host_count)
+        host_count = len(hosts)
+        step_size = max(1, host_count // 10)  # At least 1 host per tick
+        x_ticks = numpy.arange(0, host_count + step_size, step_size)
+        
+        # Create a figure and axis
+        fig, ax = mpl.subplots(figsize=(12, 6))
+
+        # Iterate through response codes and plot bars for each code
+        for i, code in enumerate(response_codes):
+            y = data_frame[code]
+            ax.bar(x, y, label=f'Statuscode {code}', color=colors[i])
+           # x += 0.15  # Increase the x position to separate bars
+
+        # Set the x-axis labels as hostnames
+        ax.set_xticks(numpy.arange(host_count))
+        ax.set_xticklabels(hosts, rotation=45, ha='right')
+
+        # Set the legend
+        ax.legend()
+
+        # Set labels and title
+        ax.set_xlabel('Hosts')
+        ax.set_ylabel('Statuscode Share')
+        ax.set_title('Distribution of Statuscodes per Host')
+
+        mpl.tight_layout()
+
+        # Save or display the plot
+        mpl.savefig(self.exp_path + '/exp_stats_host_statuscode_bars.png', dpi=300, bbox_inches='tight')
+        # mpl.show() """
     def cluster_domains(self, data_frame):
         """Figure 2"""
         selected_column = data_frame['2xx']
 
-       
         num_clusters = 10
-
- 
         mpl.figure(figsize=(10, 8))
         mpl.style.use('fivethirtyeight')
         #Weight percentage
@@ -354,6 +546,7 @@ def get_logs_directory():
 
 if __name__ == "__main__":
     log_dir=get_logs_directory()
-    path = f"{log_dir}/experiment_43"
+    #path = f"{log_dir}/experiment_43"
+    path = f"{log_dir}/extracted_logs/attic/experiment_17"
     dra = Domain_Response_Analyzator(path)
     dra.start()
